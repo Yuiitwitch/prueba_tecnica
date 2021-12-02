319722f5632ccd50039526b23f88c5df81ca3546
Alfonso Lara <86298325+Yuiitwitch@users.noreply.github.com>
2021-12-02 17:13:57 +0100

    Creamos la carpeta views y el archivo de routas para colocar los endpoints que utilizaremos mas adelante

---
+++ views/OrderRouter.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+//Importo modelo de datos
+const OrderController = require('../controllers/OrderController');
+
+// Endpoints de order.
+router.get('/', OrderController.getAll);
+router.get('/:id', OrderController.getById);
+router.post('/', OrderController.create);
+router.put('/:id', OrderController.update);
+router.delete('/:id', OrderController.delete);
+
+module.exports = router;
