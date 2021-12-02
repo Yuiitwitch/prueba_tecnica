04f8f124141a7ea5919135aa7c4d32afd777dbf6
Alfonso Lara <86298325+Yuiitwitch@users.noreply.github.com>
2021-12-03 00:27:47 +0100

    Cambio el empoint del delete , ya que no funcionaba con la ruta que tenia

--- views/OrderRouter.js
+++ views/OrderRouter.js
@@ -9,6 +9,6 @@ router.get('/', OrderController.getAll);
 router.get('/:id', OrderController.getById);
 router.post('/', OrderController.create);
 router.put('/:id', OrderController.update);
-router.delete('/:id', OrderController.delete);
+router.delete('/', OrderController.delete);
 
 module.exports = router;
